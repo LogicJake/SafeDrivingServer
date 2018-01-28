9514b79a6d8fd4a4b4a5008b0130baef98b2f05a
NepoleonAsus <835410808@qq.com>
2018-01-28 14:11:26 +0800

    上传图片 debug

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once './include/token.class.php';
 $upload_avator = '/www/driving-file/avator';
 
 // white list
-$actionList = ['postCollect','postSignup','postLogin','getForgetpasswd','verifyMailbox','postPic'];          //所有action列表
+$actionList = ['postCollect','postSignup','postLogin','getForgetpasswd','verifyMailbox','postAvator'];          //所有action列表
 
 $noTokenList = ['postCollect','postSignup','postLogin','getForgetpasswd'];         //不需要token的action
 
