d50c08334f8f97de9940fee2176787bc6852671d
NepoleonAsus <835410808@qq.com>
2018-03-31 22:33:33 +0800

    简化返回json

--- actions/postLogin.php
+++ actions/postLogin.php
@@ -4,7 +4,7 @@ require_once './include/login.function.php';
 
 if(isset($_POST['user_name'],$_POST['user_passwd'])) {
     if($_POST['user_name'] && $_POST['user_passwd']) {
-        $result['result'] = check_login(urldecode($_POST['user_name']),$_POST['user_passwd']);
+        $result = check_login(urldecode($_POST['user_name']),$_POST['user_passwd']);
         Result::success($result);
     }
 
